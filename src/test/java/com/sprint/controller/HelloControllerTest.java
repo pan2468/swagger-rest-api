9dc9c4c5334add85d00a3c97703ddebaa5a0d266
임채성 <pan1129@naver.com>
2022-10-05 16:27:18 +0900

    TDD 테스트 실행 commit

---
+++ src/test/java/com/sprint/controller/HelloControllerTest.java
@@ -0,0 +1,28 @@
+package com.sprint.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void Hello()throws Exception{
+        String hello = "hello";
+
+        mvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+}
